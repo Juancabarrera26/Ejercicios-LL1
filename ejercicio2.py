1f3418dd11bbb1c732a10945e84099a6679645a5
ProyectCyber <108159807+Juancabarrera26@users.noreply.github.com>
2026-04-14 09:53:28 -0500

    Update ejercicio2.py

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,57 +1,56 @@
 tokens = []
 pos = 0
 
+def actual():
+    if pos < len(tokens):
+        return tokens[pos]
+    return None
+
 def match(t):
     global pos
-    if pos < len(tokens) and tokens[pos] == t:
+    if actual() == t:
         pos += 1
     else:
         print("error")
         exit()
 
 def S():
-    if tokens[pos] == "dos":
+    if actual() == "dos":
         match("dos")
         C()
-    elif tokens[pos] in ["cuatro", "siete"]:
+    elif actual() in ["cuatro", "siete"]:
         B()
         match("uno")
     else:
         pass
 
 def A():
-    if tokens[pos] == "cuatro":
+    if actual() == "cuatro":
         match("cuatro")
-    elif tokens[pos] in ["dos", "siete"]:
+    elif actual() in ["dos", "siete"]:
         S()
         match("tres")
         B()
         C()
-    else:
-        pass
 
 def B():
-    if tokens[pos] in ["dos", "cuatro", "siete"]:
+    if actual() in ["dos", "cuatro", "siete"]:
         A()
         match("cinco")
         C()
         match("seis")
-    else:
-        pass
 
 def C():
-    if tokens[pos] == "siete":
+    if actual() == "siete":
         match("siete")
         B()
-    else:
-        pass
 
 
-# prueba
+# MAIN
 tokens = input("ingrese tokens: ").split()
 S()
 
 if pos == len(tokens):
     print("cadena valida")
 else:
-    print("cadena invalida (gramatica no LL1)")
+    print("cadena invalida")
